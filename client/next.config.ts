df2f48b4c39fd8b8916ed7ba6f8bc5c0cf0f9278
Khushee21 <khushirathore649@gmail.com>
2025-06-01 23:03:12 +0530

    added

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,10 +1,9 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-   images: {
+  images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
