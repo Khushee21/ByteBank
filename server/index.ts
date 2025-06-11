b4e0d1d87344ee603d091eef44b99e71aaaf3e95
Khushee21 <khushirathore649@gmail.com>
2025-06-11 23:31:56 +0530

    added

--- server/index.ts
+++ server/index.ts
@@ -15,8 +15,15 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
+
 app.use(express.json());
+
 
 app.use('/api/auth' , AuthRouter);
 app.use('/api/user' , UserProfileRouter);
